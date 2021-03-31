06afbe3e2f20a028088dd0d1db734e1ca56dfd9c
aknaim96 <39919955+aknaim96@users.noreply.github.com>
2021-03-31 11:33:12 -0400

    Update and rename SETUP.py to Scripting.py

--- Scripting.py
+++ Scripting.py
@@ -1,5 +1,5 @@
 #https://www.educative.io/edpresso/how-to-run-a-python-script-in-linux
-  
+#https://help.ubuntu.com/community/Beginners/BashScripting
 #https://askubuntu.com/questions/223691/how-do-i-create-a-script-file-for-terminal-commands
 
 #!/bin/bash
